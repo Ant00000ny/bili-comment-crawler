322621a47a3f39c7e0d285cd3b0c9c6fdda6bab1
Ant00000ny <Ant00000ny@icloud.com>
2024-04-23 14:26:56 +0800

    init

---
+++ src/main/kotlin/fun/fantasea/bilicommentcrawler/BiliCommentCrawlerApplication.kt
@@ -0,0 +1,11 @@
+package fun.fantasea.bilicommentcrawler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BiliCommentCrawlerApplication
+
+fun main(args: Array<String>) {
+    runApplication<BiliCommentCrawlerApplication>(*args)
+}
---
+++ src/test/kotlin/fun/fantasea/bilicommentcrawler/BiliCommentCrawlerApplicationTests.kt
@@ -0,0 +1,13 @@
+package fun.fantasea.bilicommentcrawler
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BiliCommentCrawlerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
